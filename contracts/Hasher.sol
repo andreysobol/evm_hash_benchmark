3999d34559d3ac602045a39829f710237234a161
Andrey Sobol <andreysobol@protonmail.com>
2021-07-01 18:25:00 +0300

    fix name

--- contracts/Hasher.sol
+++ contracts/Hasher.sol
@@ -6,7 +6,7 @@ pragma solidity >=0.7.0 <0.9.0;
  * @title Ballot
  * @dev Implements voting process along with vote delegation
  */
-contract Ballot {
+contract Hasher {
    
     bytes32[] b ;
     bytes32 sha;
