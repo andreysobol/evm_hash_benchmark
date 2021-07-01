ff1a1a44bb78d0bd72586b3b2b02de384d2b8c15
Andrey Sobol <andreysobol@protonmail.com>
2021-07-01 18:25:08 +0300

    fix

--- contracts/Hasher.sol
+++ contracts/Hasher.sol
@@ -1,11 +1,5 @@
-// SPDX-License-Identifier: GPL-3.0
-
 pragma solidity >=0.7.0 <0.9.0;
 
-/** 
- * @title Ballot
- * @dev Implements voting process along with vote delegation
- */
 contract Hasher {
    
     bytes32[] b ;
